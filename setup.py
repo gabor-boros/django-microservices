92f2f622ca39f9e4def36702bd16534e3847be71
Gábor Boros <gabor.brs@gmail.com>
2018-10-21 14:53:16 +0200

    Adjust setup file

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
 from distutils.core import setup
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='djangomicroservices',
-    version='0.1.1',
-    url='https://github.com/gabor-boros/django-microservices',
-    download_url='https://github.com/gabor-boros/django-microservices',
+    name="djangomicroservices",
+    version="0.1.1",
+    author="Gabor Boros",
+    author_email="gabor.brs@gmail.com",
     license='MIT',
-    author='Gabor Boros',
-    author_email='gabor.brs@gmail.com',
-    description='',
+    description="Simple django package to easily connect microservices.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/gabor-boros/django-microservices',
     packages=[
         'microservices',
         'microservices.management',
         'microservices.management.commands',
         'microservices.migrations'
+    ],
+    classifiers=[
+        "Programming Language :: Python"
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
